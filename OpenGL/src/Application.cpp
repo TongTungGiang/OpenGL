80736b8e0919325154eee2323e6aef871a34cfdf
Tống Tùng Giang <--local>
2019-07-09 11:20:12 +0700

    Index buffer

--- OpenGL/src/Application.cpp
+++ OpenGL/src/Application.cpp
@@ -115,18 +115,31 @@ int main(void)
 	std::cout << glGetString(GL_VERSION) << std::endl;
 
 	/* Create the vertex buffer */
-	unsigned int buffer;
-	float positions[6] = {
+	float positions[] = {
 		-0.5f, -0.5f,
-		 0.0f, 0.5f,
-		 0.5f, -0.5f
+		 0.5f, -0.5f,
+		 0.5f, 0.5f,
+		 -0.5f, 0.5f,
 	};
-	glGenBuffers(1, &buffer);
-	glBindBuffer(GL_ARRAY_BUFFER, buffer);
-	glBufferData(GL_ARRAY_BUFFER, 6 * sizeof(float), positions, GL_DYNAMIC_DRAW);
+
+	unsigned int indices[]
+	{
+		0, 1, 2,
+		2, 3, 0,
+	};
+
+	unsigned int vertexBuffer;
+	glGenBuffers(1, &vertexBuffer);
+	glBindBuffer(GL_ARRAY_BUFFER, vertexBuffer);
+	glBufferData(GL_ARRAY_BUFFER, 8 * sizeof(float), positions, GL_DYNAMIC_DRAW);
 
 	glEnableVertexAttribArray(0);
 	glVertexAttribPointer(0, 2, GL_FLOAT, GL_FALSE, 2 * sizeof(float), 0);
+
+	unsigned int indexBuffer;
+	glGenBuffers(1, &indexBuffer);
+	glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, indexBuffer);
+	glBufferData(GL_ELEMENT_ARRAY_BUFFER, 6 * sizeof(unsigned int), indices, GL_DYNAMIC_DRAW);
 
 	/* Shader */
 	ShaderSource source = ParseShader("res/shaders/Basic.shader");
@@ -140,7 +153,7 @@ int main(void)
 		glClear(GL_COLOR_BUFFER_BIT);
 
 		/* Make the draw call */
-		glDrawArrays(GL_TRIANGLES, 0, 3);
+		glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, nullptr);
 
 		/* Swap front and back buffers */
 		glfwSwapBuffers(window);
