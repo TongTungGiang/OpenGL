b0770a7e078ca5fe91deb990ede8875246a28ea6
Tống Tùng Giang <tongtunggiang@gmail.com>
2019-07-12 11:48:05 +0700

    Setting up Projection Matrix to display the texture correctly in 4:3 ratio

--- OpenGL/src/Application.cpp
+++ OpenGL/src/Application.cpp
@@ -11,6 +11,9 @@
 #include "Shader.h"
 #include "Texture.h"
 
+#include "glm/glm.hpp"
+#include "glm/gtc/matrix_transform.hpp"
+
 int main(void)
 {
 	GLFWwindow* window;
@@ -74,7 +77,9 @@ int main(void)
 	Shader shader("res/shaders/Basic.shader");
 	shader.Bind();
 
-	shader.SetUniform4f("u_Color", 1.0f, 0.5f, 0.0f, 1.0f);
+	/* Projection matrix for 4:3 ratio */
+	glm::mat4 proj = glm::ortho(-2.0f, 2.0f, -1.5f, 1.5f, -1.0f, 1.0f);
+	shader.SetUniformMat4f("u_MVP", proj);
 
 	/* Texture */
 	Texture texture("res/textures/skull.png");
--- OpenGL/src/Shader.h
+++ OpenGL/src/Shader.h
@@ -3,6 +3,8 @@
 #include <unordered_map>
 #include <string>
 
+#include <glm/glm.hpp>
+
 struct ShaderSource
 {
 	std::string VertexSource;
@@ -22,6 +24,7 @@ public:
 	void SetUniform4f(const std::string& name, float v0, float v1, float v2, float v3);
 	void SetUniform1f(const std::string& name, float v);
 	void SetUniform1i(const std::string& name, int v);
+	void SetUniformMat4f(const std::string& name, const glm::mat4& matrix);
 
 private:
 
